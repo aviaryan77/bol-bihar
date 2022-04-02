474114c402da2a87bee44d7111068abe9e63127d
aviaryan77 <83573216+avinasharyan614@users.noreply.github.com>
2022-04-03 03:15:32 +0530

    first comit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
-const Home: NextPage = () => {
+const HomePage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -83,4 +82,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default HomePage
