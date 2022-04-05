d47c59f7f6aaaf8ea608f703830ea60127505f40
aviaryan77 <83573216+avinasharyan614@users.noreply.github.com>
2022-04-05 17:49:11 +0530

    added category slug

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import Link from 'next/link'
-import { getCategories } from '@/services'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getCategories } from '@/services';
 
 function Header() {
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
-    getCategories().then((newCategories) => setCategories(newCategories))
-  }, [])
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
 
   return (
     <div className="container mx-auto mb-8 px-10">
@@ -19,8 +19,8 @@ function Header() {
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-          {categories.map((category) => (
-            <Link key={category.slug} href={`/category/${category.slug}`}>
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
               <span className="mt-2 ml-4 align-middle text-white md:float-right">
                 {category.name}
               </span>
@@ -29,7 +29,7 @@ function Header() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
